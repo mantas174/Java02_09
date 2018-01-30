0d0e564c13ea3e6071267b6a21ea5193ab396cd7
Mantas <mantas.muzzz@gmail.com>
2018-01-30 15:56:50 +0200

    Padariau

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 
 public class Main {
 
+    static  float km, sanaudos;
+
     public static void main(String[] args) {
     /*  Realizuoti programą, kuri apskaičiuotų kiek automobilis vidutiniškai sunaudoja kuro 100
         km. Programa turi paprašyti vartotojo įvesti nuvažiuotus km ir kuro sąnaudas.
@@ -13,15 +15,20 @@ public class Main {
         Papildyti 1.8 programą, parašykite du atskirus metodus, vienas turi nuskaityti įvestus
         vartotojo parametrus, kitas paskaičiuotų ir grąžintų vidurkį. Rezultatus spausdinti ekrane.
      */
-        float km, sanaudos;
+
+        nuskaitymas();
+        System.out.print("Vidurkis: " + vidurkis());
+
+    }
+    public static void nuskaitymas(){
         Scanner in = new Scanner(System.in);
 
         System.out.print("Iveskite nuvaziuotus kilometrus: ");
         km = in.nextFloat();
         System.out.print("Iveskite Kuro sanaudas: ");
         sanaudos = in.nextFloat();
-
-        System.out.print("Vidurkis: " + (sanaudos * 100) / km);
-
+    }
+    public static float vidurkis(){
+        return (sanaudos * 100) / km;
     }
 }
